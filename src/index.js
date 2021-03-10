ff104350cb1faec9bd2ff5513182b514b8addb77
annie2527 <you@example.com>
2021-03-10 23:46:59 +0100

    navbar beginning

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from 'react-router-scroll-top';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
