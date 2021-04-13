c75e9789e815c9db8123839d0aa015207dad9a03
annie2527 <you@example.com>
2021-04-13 22:58:26 +0200

    ams section

--- src/components/homepage/ams/AMS.js
+++ src/components/homepage/ams/AMS.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {AMSData} from './AMSData';
+import './ams.scss';
 
 export const AMS = () => {
     return (
-        <div>
-            <h2>{AMSData.title}</h2>
+        <div className='ams-container'>
+            <div data-aos="fade-right" className='ams-img-container'>
+                <img src={AMSData.image} alt='Circular Cycle'/>
+            </div>
+            <div data-aos="fade-left" className='ams-text-container'>
+                <h2>{AMSData.title}</h2>
+                <h5>{AMSData.date}</h5>
+                <p>{AMSData.description}</p>
+            </div>   
         </div>
     )
 }
